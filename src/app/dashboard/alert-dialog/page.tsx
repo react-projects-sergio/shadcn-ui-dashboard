2187fcc1b386d3e9fcaa5d08e340041bb74f1c05
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-15 09:47:44 -0700

    alert dialog component

---
+++ src/app/dashboard/alert-dialog/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function Page() {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  return (
+    <div>
+      <AlertDialog
+        open={dialogOpen}
+        // onOpenChange={(open) => console.log({ open })}
+        onOpenChange={setDialogOpen}
+      >
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => console.log("cancel")}>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={() => console.log("continue")}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+}
